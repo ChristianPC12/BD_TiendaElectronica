0b212a9908470777586987d88e5e0db92a809dcd
Christian12 <crisp314528@gmail.com>
2025-05-06 14:42:04 -0600

    Este es el commit inicial para comprobar que todo esté bien.

---
+++ src/Prueba.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Crisp
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //hola, esto es una prueba para comprobar que todo se inició bien
+    }
+    
+}
