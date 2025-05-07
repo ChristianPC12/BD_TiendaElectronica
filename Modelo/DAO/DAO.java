0f67725f581f50d45227888fc1c3c2a0e585416f
Christian12 <crisp314528@gmail.com>
2025-05-07 14:34:44 -0600

    Añadida clase abstracta DAO con métodos CRUD genéricos

---
+++ Modelo/DAO/DAO.java
@@ -0,0 +1,23 @@
+
+package Modelo.DAO;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+/**
+ *
+ * @author Christian Paniagua Castro
+ */
+public abstract class DAO<Dto> {
+    protected Connection connection;
+
+    public DAO(Connection connection) {
+        this.connection = connection;
+    }
+    
+    public abstract boolean create (Dto dto)throws SQLException;
+    public abstract Dto read (Object id)throws SQLException;
+    public abstract List<Dto> readAll ()throws SQLException;
+    public abstract boolean update (Dto dto)throws SQLException;
+    public abstract boolean delete (Object id)throws SQLException;
+    
+}
